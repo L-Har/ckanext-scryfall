eaf409dc1386b78e95ea6982abab809143b7d7e0
= <=>
2024-07-09 15:50:46 -0500

    extend IPackageController barebones

--- ckanext/scryfall/plugin.py
+++ ckanext/scryfall/plugin.py
@@ -1,16 +1,41 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-
 class ScryfallPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPackageController)
     
+    read(self, entity):
+        pass
 
-    # IConfigurer
+    create(self, entity):
+        pass
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "scryfall")
+    edit(self, entity):
+        pass
 
-    
+    delete(self, entity):
+        pass
+
+    after_create(self, context, pkg_dict):
+        pass
+
+    after_update(self, context, pkg_dict):
+        pass
+
+    after_delete(self, context, pkg_dict):
+        pass
+
+    after_show(self, context, pkg_dict):
+        pass
+
+    before_search(self, search_params):
+        pass
+
+    after_search(self, search_results, search_params):
+        pass
+
+    before_index(self, pkg_dict):
+        pass
+
+    before_view(self, pkg_dict):
+        pass
