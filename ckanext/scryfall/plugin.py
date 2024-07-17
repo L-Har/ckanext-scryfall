050b47954b48a510b250410ea5e258021444b57f
= <=>
2024-07-17 11:12:18 -0500

    in progress plugin active dev

--- ckanext/scryfall/plugin.py
+++ ckanext/scryfall/plugin.py
@@ -4,38 +4,41 @@ import ckan.plugins.toolkit as toolkit
 class ScryfallPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IPackageController)
     
-    read(self, entity):
+    def read(self, entity):
         pass
 
-    create(self, entity):
+    def create(self, entity):
         pass
 
-    edit(self, entity):
+    def edit(self, entity):
         pass
 
-    delete(self, entity):
+    def delete(self, entity):
         pass
 
-    after_create(self, context, pkg_dict):
+    def after_create(self, context, pkg_dict):
         pass
 
-    after_update(self, context, pkg_dict):
+    def after_update(self, context, pkg_dict):
         pass
 
-    after_delete(self, context, pkg_dict):
+    def after_delete(self, context, pkg_dict):
         pass
 
-    after_show(self, context, pkg_dict):
+    def after_show(self, context, pkg_dict):
         pass
 
-    before_search(self, search_params):
+    def before_dataset_search(self, search_params):
         pass
 
-    after_search(self, search_results, search_params):
+    def before_search(self, search_params):
         pass
 
-    before_index(self, pkg_dict):
+    def after_search(self, search_results, search_params):
         pass
 
-    before_view(self, pkg_dict):
+    def before_index(self, pkg_dict):
+        pass
+
+    def before_view(self, pkg_dict):
         pass
