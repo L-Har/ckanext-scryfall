a8cd72a1010c36c534bbb426e8ae8e093c391b1d
= <=>
2024-07-17 12:14:56 -0500

    test post commit script

--- ckanext/scryfall/plugin.py
+++ ckanext/scryfall/plugin.py
@@ -13,48 +13,46 @@ class ScryfallPlugin(plugins.SingletonPlugin):
         """create"""
         print("In ")
 
-
     def edit(self, entity):
         """edit"""
         print("In ")
-        
-    def delete(self, entity   ):
+
+    def delete(self, entity      ):
         """delete"""
         print("In ")
-        
+
     def after_create(self, context, pkg_dict):
         """after_create"""
         print("In ")
-        
+
     def after_update(self, context, pkg_dict):
         """after_update"""
         print("In ")
-        
-    def after_delete( self, context, pkg_dict):
+
+    def after_delete(self, context, pkg_dict):
         """after_delete"""
         print("In ")
-        
+
     def after_show(self, context, pkg_dict):
         """after_show"""
         print("In ")
-        
-    def before_dataset_search( self, search_params):
+
+    def before_dataset_search(self, search_params):
         """before_dataset_search"""
         print("In ")
-        
+
     def before_search(self, search_params):
         """before_search"""
         print("In ")
-        
+
     def after_search(self, search_results, search_params):
         """after_search"""
         print("In ")
-        
+
     def before_index(self, pkg_dict):
         """before_index"""
         print("In ")
-        
+
     def before_view(self, pkg_dict):
         """before_view"""
         print("In ")
-        
