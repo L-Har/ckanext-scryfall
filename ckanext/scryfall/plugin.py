fdef6100320d0d8286a82633b76e0de7faa4e0e6
lh <111371248+L-Har@users.noreply.github.com>
2024-07-18 11:33:42 -0500

    Implement IResourceView, Add info method.

--- ckanext/scryfall/plugin.py
+++ ckanext/scryfall/plugin.py
@@ -1,112 +1,105 @@
-from typing import Any
-from ckan.types import Context
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+"""
+A plugin to connect to the scryfall api from CKAN.
+"""
+
+from ckan import plugins
+from ckan.types import Any, Context, DataDict
+from ckan.plugins import toolkit
 
 
 class ScryfallPlugin(plugins.SingletonPlugin):
-    """Class ScryfallPlugin implements IPackageController"""
+    """Class ScryfallPlugin implements IResourceView"""
 
-    plugins.implements(plugins.IPackageController)
+    plugins.implements(plugins.IResourceView)
 
-    def read(self, entity: "model.Package") -> None:
+    # Add custom view renderings for different resource types.
+    def info(self) -> dict[str, Any]:
+        """Returns a dictionary with configuration options for the Scryfall Plugin."""
+        scryfall_config = {
+            "name": "scryfall",
+            "title": toolkit._("Scryfall Viewer"),
+            "default_title": toolkit._("Scryfall View"),
+            "default_description": toolkit._(
+                "This is a viewer to the Magic The Gathering Scryfall public API."
+            ),
+            "icon": "magic",
+            "always_available": True,
+            "iframed": True,
+            "preview_enabled": False,
+            "full_page_edit": False,
+        }
+
+        return scryfall_config
+
+    def can_view(self, data_dict: DataDict) -> bool:
         """
-        Called after IPackageController.before_dataset_view inside
-        package_show.
+        Returns whether the plugin can render a particular resource.
+
+        The ``data_dict`` contains the following keys:
+
+        :param resource: dict of the resource fields
+        :param package: dict of the full parent dataset
+
+        :returns: True if the plugin can render a particular resource, False
+            otherwise
+        :rtype: bool
         """
+        return False
 
-    def create(self, entity: "model.Package"):
-        """Called after the dataset had been created inside package_create."""
-
-    def edit(self, entity: "model.Package") -> None:
-        """Called after the dataset had been updated inside package_update."""
-
-        print("Editing action")
-
-    def delete(self, entity: "model.Package") -> None:
-        """Called before commit inside package_delete."""
-
-    def after_dataset_create(self, context: Context, pkg_dict: dict[str, Any]) -> None:
-        """
-        Extensions will receive the validated data dict after the dataset
-        has been created (Note that the create method will return a dataset
-        domain object, which may not include all fields). Also the newly
-        created dataset id will be added to the dict.
-        """
-
-    def after_dataset_update(self, context: Context, pkg_dict: dict[str, Any]) -> None:
-        """
-        Extensions will receive the validated data dict after the dataset
-        has been updated.
-
-        Note that bulk dataset update actions (`bulk_update_private`,
-        `bulk_update_public`) will bypass this callback. See
-        ``ckan.plugins.toolkit.chained_action`` to wrap those actions
-        if required.
-        """
-
-    def after_dataset_delete(self, context: Context, pkg_dict: dict[str, Any]) -> None:
-        """
-        Extensions will receive the data dict (typically containing
-        just the dataset id) after the dataset has been deleted.
-
-        Note that the `bulk_update_delete` action will bypass this
-        callback. See ``ckan.plugins.toolkit.chained_action`` to wrap
-        that action if required.
-        """
-
-    def after_dataset_show(self, context: Context, pkg_dict: dict[str, Any]) -> None:
-        """
-        Extensions will receive the validated data dict after the dataset
-        is ready for display.
-        """
-
-    def before_dataset_search(self, search_params: dict[str, Any]) -> dict[str, Any]:
-        """
-        Extensions will receive a dictionary with the query parameters,
-        and should return a modified (or not) version of it.
-
-        search_params will include an `extras` dictionary with all values
-        from fields starting with `ext_`, so extensions can receive user
-        input from specific fields.
-        """
-        return search_params
-
-    def after_dataset_search(
-        self, search_results: dict[str, Any], search_params: dict[str, Any]
+    def setup_template_variables(
+        self, context: Context, data_dict: DataDict
     ) -> dict[str, Any]:
         """
-        Extensions will receive the search results, as well as the search
-        parameters, and should return a modified (or not) object with the
-        same structure::
+        Adds variables to be passed to the template being rendered.
 
-            {'count': '', 'results': '', 'search_facets': ''}
+        This should return a new dict instead of updating the input
+        ``data_dict``.
 
-        Note that count and facets may need to be adjusted if the extension
-        changed the results for some reason.
+        The ``data_dict`` contains the following keys:
 
-        search_params will include an `extras` dictionary with all values
-        from fields starting with `ext_`, so extensions can receive user
-        input from specific fields.
+        :param resource_view: dict of the resource view being rendered
+        :param resource: dict of the parent resource fields
+        :param package: dict of the full parent dataset
 
+        :returns: a dictionary with the extra variables to pass
+        :rtype: dict
         """
+        return {}
 
-        return search_results
+    def view_template(self, context: Context, data_dict: DataDict) -> str:
+        """
+        Returns a string representing the location of the template to be
+        rendered when the view is displayed
 
-    def before_dataset_index(self, pkg_dict: dict[str, Any]) -> dict[str, Any]:
-        """
-        Extensions will receive what will be given to Solr for
-        indexing. This is essentially a flattened dict (except for
-        multi-valued fields such as tags) of all the terms sent to
-        the indexer. The extension can modify this by returning an
-        altered version.
-        """
-        return pkg_dict
+        The path will be relative to the template directory you registered
+        using the :py:func:`~ckan.plugins.toolkit.add_template_directory`
+        on the :py:class:`~ckan.plugins.interfaces.IConfigurer.update_config`
+        method, for instance ``views/my_view.html``.
 
-    def before_dataset_view(self, pkg_dict: dict[str, Any]) -> dict[str, Any]:
+        :param resource_view: dict of the resource view being rendered
+        :param resource: dict of the parent resource fields
+        :param package: dict of the full parent dataset
+
+        :returns: the location of the view template.
+        :rtype: string
         """
-        Extensions will receive this before the dataset gets
-        displayed. The dictionary passed will be the one that gets
-        sent to the template.
+        return ""
+
+    def form_template(self, context: Context, data_dict: DataDict) -> str:
         """
-        return pkg_dict
+        Returns a string representing the location of the template to be
+        rendered when the edit view form is displayed
+
+        The path will be relative to the template directory you registered
+        using the :py:func:`~ckan.plugins.toolkit.add_template_directory`
+        on the :py:class:`~ckan.plugins.interfaces.IConfigurer.update_config`
+        method, for instance ``views/my_view_form.html``.
+
+        :param resource_view: dict of the resource view being rendered
+        :param resource: dict of the parent resource fields
+        :param package: dict of the full parent dataset
+
+        :returns: the location of the edit view form template.
+        :rtype: string
+        """
+        return ""
